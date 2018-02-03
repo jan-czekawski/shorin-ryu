1575fc09dcdb881cf092a862c1328e84e8af2d82
Michal <bihone88@gmail.com>
2018-02-03 13:12:29 +0000

    Add first user validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe User, ".new" do
+    
+    it "creates new user if all info is provided" do
+      # setup
+      old_count = User.all.count
+      
+      # exercise
+      User.create(email: "user@email.com", password: "password")
+      new_count = User.all.count
+      
+      # verify
+      expect(new_count).to eq old_count + 1
+      
+      # teardown
+      User.all.delete_all
+    end
+  
+  end
 end
