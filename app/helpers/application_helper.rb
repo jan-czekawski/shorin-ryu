6573c48987e540dd9ca5a1021802a77ce0a08d48
Michal <bihone88@gmail.com>
2018-02-06 12:04:39 +0000

    Style users index page, add links to edit and show user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def show_avatar(user, size = 100)
     id = Digest::MD5.hexdigest(user.email.downcase)
     path = "https://secure.gravatar.com/avatar/#{id}?s=#{size}&d=mm"
-    image_tag(path, alt: user.name)
+    image_tag(path, alt: user.email, class: "avatar")
   end
 end
