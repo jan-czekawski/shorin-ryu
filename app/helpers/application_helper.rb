d16ba1b9bc4395373493f7a0e40e2b340e174510
Michal <bihone88@gmail.com>
2018-02-06 11:42:24 +0000

    Add show gravatar method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def show_avatar(user, size = 100)
+    id = Digest::MD5.hexdigest(user.email.downcase)
+    path = "https://secure.gravatar.com/avatar/#{id}?s=#{size}&d=mm"
+    image_tag(path, alt: user.name)
+  end
 end
