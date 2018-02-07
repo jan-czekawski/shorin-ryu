f10b5fa1c81868d94bcb2db3e59ebac79b3248ac
Michal <bihone88@gmail.com>
2018-02-07 01:31:39 +0000

    Update user destroy action and update show user link in navbar and index user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,27 @@
 class UsersController < ApplicationController
   before_action :logged_in?, only: [:index]
+  before_action :set_user, only: [:show, :destroy]
   
   def index
     @users = User.all
   end
   
   def show
-    @user = User.find(params[:id])
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:notice] = "User was deleted!"
+    redirect_to root_path
   end
   
   private
   
   def logged_in?
     redirect_to root_path unless user_signed_in?
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
