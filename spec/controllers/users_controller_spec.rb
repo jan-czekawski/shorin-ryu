f2ad0946bd1831822252064a00ea8f8139a91419
Michal <bihone88@gmail.com>
2018-02-07 11:00:42 +0000

    Add users index controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,9 +2,21 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe "Users#index" do
-    it "redirects from index unless user logged in" do
+    
+    before(:all) do
+      @user = create(:user, email: "controller@email.com")
+    end
+    
+    it "redirects from index unless user's logged in" do
       get :index
       expect(response).to redirect_to(controller: "pages", action: "index")
+    end
+    
+    it "renders index template if user's logged in" do
+      sign_in @user
+      get :index
+      expect(response.status).to eq(200)
+      expect(response).to render_template("index")
     end
   end
 end
