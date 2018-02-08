9bf26a7aa4bad409fe161eece50fa929102264bf
Michal <bihone88@gmail.com>
2018-02-08 20:07:14 +0000

    Define index and show methods in event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,13 @@
 class EventsController < ApplicationController
+  before_action :set_event, except: [:index]
+  
+  def index
+    @events = Event.all
+  end
+  
+  def show; end
+    
+  def set_event
+    @event = Event.find(params[:id])
+  end
 end
