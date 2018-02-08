00aaf6439ae26e12f36182edd1db44efb254ad85
Michal <bihone88@gmail.com>
2018-02-08 18:32:55 +0000

    Remove unecessary specs

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe EventsController, type: :controller do
 
   describe "GET #name:string" do
     it "returns http success" do
-      get :name:string
-      expect(response).to have_http_status(:success)
+      # get :name:string
+      # expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #address:object" do
     it "returns http success" do
-      get :address:object
-      expect(response).to have_http_status(:success)
+      # get :address:object
+      # expect(response).to have_http_status(:success)
     end
   end
 
