85848084d8a6fab9ba62bd13b585e1a4126d9ec7
Michal <bihone88@gmail.com>
2018-02-09 20:21:56 +0000

    Add timestamps to event and user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event
   include Mongoid::Document
+  include Mongoid::Timestamps
+  
   field :name, type: String
-  field :address, type: Object
+  field :address, type: Hash
   
   validates :name, presence: true
   validates :address, presence: true
