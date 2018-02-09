34cf58f9e817fd6b6a64f8e362b3078586341954
Michal <bihone88@gmail.com>
2018-02-09 18:47:01 +0000

    Add event destroy spec, update event show template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def home; end
+
+  def require_user
+    return if user_signed_in?
+    flash[:alert] = "You must be logged in to do that!"
+    redirect_to root_path
+  end
+  
+  def require_admin
+    return if current_user.admin?
+    flash[:alert] = "You must be an admin to do that!"
+    redirect_to root_path
+  end
+
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -52,16 +52,4 @@ class EventsController < ApplicationController
                                                    :flat_number,
                                                    :zip_code])
   end
-  
-  def require_user
-    return if user_signed_in?
-    flash[:alert] = "You must be logged in to do that!"
-    redirect_to root_path
-  end
-  
-  def require_admin
-    return if current_user.admin?
-    flash[:alert] = "You must be an admin to do that!"
-    redirect_to root_path
-  end
 end
