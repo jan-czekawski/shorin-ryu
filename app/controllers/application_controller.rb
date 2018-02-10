afc3b9c2b3776be3002289b3460de733ab22b0eb
Michal <bihone88@gmail.com>
2018-02-10 15:16:28 +0000

    Style home page and flash at the home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_class, except: [:home]
+  before_action :set_flash, only: [:home]
   
   def home; end
 
@@ -18,6 +19,11 @@ class ApplicationController < ActionController::Base
   
   def set_class
     @body_class = "container"
+  end
+  
+  def set_flash
+    @flash_row = "row justify-content-center"
+    @flash_col = "col-10 absolute"
   end
 
 end
