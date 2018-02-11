00bd19e41b774d7b82393afea19c59c914be0f0d
Michal <bihone88@gmail.com>
2018-02-11 00:27:48 +0000

    Update db/seeds to generate fake events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
+# users = []
+# 10.times do
+#   users << "#{Faker::Name.first_name}@gmail.com"
+# end
 
-users = []
-100.times do
-  users << "#{Faker::Name.first_name}@gmail.com"
-end
+# users.uniq!
 
-users.uniq!
+# users.each do |user|
+#   User.create(email: user, login: user.slice(/\w+(?=@)/),
+#               password: "password", password_confirmation: "password")
+# end
 
-users.each do |user|
-  User.create(email: user, login: user.slice(/\w+(?=@)/),
-              password: "password", password_confirmation: "password")
+10.times do
+  User.all
+      .sample
+      .events
+      .create(name: Faker::SiliconValley.company,
+              address: { city: Faker::Witcher.location,
+                         street: Faker::Address.street_name,
+                         house_number: Faker::Address.building_number,
+                         zip_code: Faker::Address.postcode })
 end
