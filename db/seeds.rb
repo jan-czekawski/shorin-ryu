8189b4ad216f1e9cbcfce52bee2dfbeff945854b
Michal <bihone88@gmail.com>
2018-02-11 14:34:38 +0000

    Update db seeds file - fix city name, street number, house number and zip code generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,25 @@
-# users = []
-# 10.times do
-#   users << "#{Faker::Name.first_name}@gmail.com"
-# end
+users = []
+10.times do
+  users << "#{Faker::Name.first_name}@gmail.com"
+end
 
-# users.uniq!
+users.uniq!
 
-# users.each do |user|
-#   User.create(email: user, login: user.slice(/\w+(?=@)/),
-#               password: "password", password_confirmation: "password")
-# end
+users.each do |user|
+  User.create(email: user, login: user.slice(/\w+(?=@)/),
+              password: "password", password_confirmation: "password")
+end
 
 10.times do
+  zip_first = format("%.2d", rand(100))
+  zip_second = format("%.3d", rand(1000))
+  
   User.all
       .sample
       .events
-      .create(name: Faker::SiliconValley.company,
-              address: { city: Faker::Witcher.location,
+      .create(name: Faker::App.name,
+              address: { city: Faker::Address.city,
                          street: Faker::Address.street_name,
-                         house_number: Faker::Address.building_number,
-                         zip_code: Faker::Address.postcode })
+                         house_number: rand(100),
+                         zip_code: "#{zip_first}-#{zip_second}" })
 end
