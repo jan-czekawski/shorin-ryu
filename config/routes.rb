f3793e7a0f3547ea67bd0580a1cc3ae263343c7d
Michal <bihone88@gmail.com>
2018-02-12 17:51:01 +0000

    Generate items controller, update routes and delete unecessary specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
-
   devise_for :users
   resources "users", only: [:index, :show, :destroy]
   resources "events"
+  resources "items"
   root "application#home"
   get "/home", to: "application#home", as: "home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
