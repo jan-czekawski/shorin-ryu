7f8d06b0a5cce486d9a210a5311741a5ee5ec121
Michal <bihone88@gmail.com>
2018-02-12 18:40:33 +0000

    Update item model - change type double to float on field size

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item
   field :name, type: String
   field :description, type: String
   field :size, type: Hash
-  field :price, type: Double
+  field :price, type: Float
   field :item_id, type: Integer
   field :picture, type: String
 end
