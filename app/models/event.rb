d1a9ed56cb9b06cb3856d8bc8bfea43afaedc770
Michal <bihone88@gmail.com>
2018-02-14 19:10:43 +0000

    Add image to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,14 @@
 class Event
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  attr_accessor :image, :image_cache 
+  mount_uploader :image, ImageUploader
   
   belongs_to :user
   
-  field :name, type: String
+  field :name,  type: String
+  field :image, type: String, default: ""
   # field :address, type: Hash
   embeds_one :address
   accepts_nested_attributes_for :address
