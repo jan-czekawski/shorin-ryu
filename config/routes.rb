3de1790d1f8826b4913c804b90ddcfef7cb27c3c
Michal <bihone88@gmail.com>
2018-02-14 19:53:27 +0000

    Update routes - nested comments for events and items

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources "users", only: [:index, :show, :destroy]
-  resources "events"
-  resources "items"
+  resources "events", "items" do
+    resources "comments", only: [:create, :update, :destroy]
+  end
   root "application#home"
   get "/home", to: "application#home", as: "home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
 end
