b8f36d06042090efb57054d9a5252ba84bf0ed54
Michal <bihone88@gmail.com>
2018-02-14 20:19:06 +0000

    Update comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment
   include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true
+  
+  field "content", type: String
 end
