822b95f81493eabb761643da01eb065ef9b90ebd
Michal <bihone88@gmail.com>
2018-02-14 22:28:41 +0000

    Update comments controller and router

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
+  # before_action :require_user
+  before_action :set_comment, only: [:create, :update, :destroy]
+  
+  def index
+    @comments = Comment.all
+  end
   
   def create
   end
@@ -7,5 +13,9 @@ class CommentsController < ApplicationController
   end
   
   def destroy
+  end
+  
+  def set_comment
+    @comment = params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources "events", "items" do
     resources "comments", only: [:create, :update, :destroy]
   end
+  get "/comments", to: "comments#index"
   root "application#home"
   get "/home", to: "application#home", as: "home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
