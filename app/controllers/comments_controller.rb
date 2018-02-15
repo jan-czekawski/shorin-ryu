9efb23c6711f02f569f38c56df993561f015b024
Michal <bihone88@gmail.com>
2018-02-15 10:12:23 +0000

    Add create comment form in events index page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,15 @@
 class CommentsController < ApplicationController
   # before_action :require_user
-  before_action :set_comment, only: [:create, :update, :destroy]
+  before_action :set_comment, only: [:destroy] #,:create, :update, ]
+  before_action :require_user, only: [:create]
+  before_action :set_event, only: [:create]
   
   def index
     @comments = Comment.all
   end
   
   def create
+    @event.comments.create(comment_params)
   end
   
   def update
@@ -18,4 +21,11 @@ class CommentsController < ApplicationController
   def set_comment
     @comment = params[:id]
   end
+  
+  def comment_params
+    params.require(:comment).permit(:content, :commentable_type, :commentable_id, :user)
+  end
 end
+
+
+
