76f376100232c9c66de112755a54bcf721d2d7af
Michal <bihone88@gmail.com>
2018-02-18 16:03:46 +0000

    Add rescuing from document not found in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :destroy]
   before_action :require_user, only: [:index, :destroy]
   before_action :require_admin, only: [:destroy]
+  rescue_from Mongoid::Errors::DocumentNotFound, with: :wrong_user
   
   def index
     @users = User.all
@@ -19,6 +20,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+  
+  def wrong_user
+    logger.error "Can't access invalid user #{params[:id]}"
+    flash[:danger] = "Invalid user."
+    redirect_to users_path
   end
   
 end
