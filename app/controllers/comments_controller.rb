3ab077b8fcbc44dea633270305164bb8159866e9
Michal <bihone88@gmail.com>
2018-02-22 19:02:36 +0000

    Update comments create method to correctly add comments to items or events.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,24 @@ class CommentsController < ApplicationController
   # before_action :require_user
   before_action :set_comment, only: [:destroy] #,:create, :update, ]
   before_action :require_user, only: [:create]
-  before_action :set_event, only: [:create]
+  # before_action :set_event, only: [:create]
   
   def index
     @comments = Comment.all
   end
   
   def create
-    @comment = @event.comments.build(comment_params)
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
       flash[:info] = "New comment has been added!"
-      redirect_to events_path
+      # TODO: change it
+      redirect_to @commentable
     else
       flash[:alert] = "There was an error!" + @comment.errors.full_messages.to_s
-      redirect_to events_path
+      # TODO: change it
+      redirect_to @commentable
     end
   end
   
@@ -26,18 +29,22 @@ class CommentsController < ApplicationController
   def destroy
   end
   
+  def find_commentable
+    params.each do |name, value| 
+      if name =~ (/(.+)_id/)
+        return $1.classify.constantize.find(value)
+      end
+    end
+  end
+  
   def set_comment
     @comment = params[:id]
   end
   
   def comment_params
     params.require(:comment).permit(:content)
-    # params.require(:comment).permit(:content).permit(:user_id, :event_id)
   end
   
-  def set_event
-    @event = Event.find(params[:event_id])
-  end
 end
 
 
