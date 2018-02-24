41b9648eae5ca1a2ffce5ed91d7c78fad054e6a7
Michal <bihone88@gmail.com>
2018-02-24 18:47:48 +0000

    Finish item model spec and update event model spec.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,23 +3,19 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   describe Event, ".new" do
     
-    after(:all) do
-      User.delete_all
-    end
-    
     it "creates valid event" do
-      user = create(:user, email: "event@associate.com", login: "event")
-      event = build(:event, user_id: user.id)
+      event = build(:event)
       expect(event).to be_valid
     end
     
     it "requires name" do
-      event = build(:event, user_id: 1, name: nil)
+      event = build(:event, name: nil)
       expect(event).not_to be_valid
     end
   
     it "requires address" do
-      event = build(:event, user_id: 1, address: nil)
+      event = build(:event)
+      event.address = nil
       expect(event).not_to be_valid
     end
     
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe Item, ".new" do
+    
+    after(:all) do
+      Item.delete_all
+    end
+    
+    it "creates valid item if all info is provided" do
+      item = build(:item)
+      expect(item).to be_valid
+    end
+    
+    it "requires name" do
+      item = build(:item, name: nil)
+      expect(item).not_to be_valid
+    end
+    
+    it "requires description" do
+      item = build(:item, description: nil)
+      expect(item).not_to be_valid
+    end
+    
+    it "requires price" do
+      item = build(:item, price: nil)
+      expect(item).not_to be_valid
+    end
+    
+    it "requires size" do
+      item = build(:item, size: nil)
+      expect(item).not_to be_valid
+    end
+    
+    it "requires quantity" do
+      item = build(:item, quantity: nil)
+      expect(item).not_to be_valid
+    end
+    
+    it "requires store_item_id" do
+      item = build(:item, store_item_id: nil)
+      expect(item).not_to be_valid
+    end
+    
+    it "requires price greater or equal to 0.01" do
+      item = build(:item, price: 0.001)
+      expect(item).not_to be_valid
+    end
+    
+    it "requires size in correct format: xs, s, m, l, xl, xxl" do
+      item = build(:item, size: "xxxl")
+      expect(item).not_to be_valid
+    end
+    
+    it "requires price greater or equal to 0.01" do
+      item = build(:item, price: 0.001)
+      expect(item).not_to be_valid
+    end
+    
+    it "requires unique name" do
+      item = create(:item)
+      second_item = build(:item)
+      expect(second_item).not_to be_valid
+    end
+    
+    it "requires unique store_item_id" do
+      item = create(:item, store_item_id: 100, name: "random")
+      second_item = build(:item, store_item_id: 100, name: "another")
+      expect(second_item).not_to be_valid
+    end
+  end
 end
