211d05061800b006ae262ec7a7e4e309c27bb114
Michal <bihone88@gmail.com>
2018-02-25 18:43:27 +0000

    Add #new to devise registration controller spec

--- spec/controllers/devise_registrations_controller_spec.rb
+++ spec/controllers/devise_registrations_controller_spec.rb
@@ -10,20 +10,42 @@ RSpec.describe Devise::RegistrationsController, type: :controller do
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
   
-  describe "Devise::RegistrationsController#create" do
-    it "creates new user if valid information is provided" do
-      expect do
-        post :create, params: { user: attributes_for(:user) }
-      end.to change(User, :count).by(1)
-      expect(response).to redirect_to(root_url)
+  describe "RegistrationsController#new" do
+    context "when user not logged in" do
     end
     
-    it "doesn't create new user and redirects if invalid info is provided" do
-      attributes = attributes_for(:user, email: nil)
-      expect do
-        post :create, params: { user: attributes }
-      end.not_to change(User, :count)
-      expect(response).to render_template("new")
+    context "when user logged in" do
+      it "redirects to root url" do
+        sign_in create(:user)
+      end
+    end
+  end
+  
+  describe "RegistrationsController#create" do
+    context "with valid information" do
+      it "increases users count by 1" do
+        expect do
+          post :create, params: { user: attributes_for(:user) }
+        end.to change(User, :count).by(1)
+      end
+      
+      it "redirects to root after save" do
+        post :create, params: { user: attributes_for(:user) }
+        expect(response).to redirect_to(root_url)
+      end
+    end
+    
+    context "with invalid information" do
+      it "doesn't change users count" do
+        expect do
+          post :create, params: { user: attributes_for(:user, email: nil) }
+        end.not_to change(User, :count)
+      end
+      
+      it "renders template 'new'" do
+        post :create, params: { user: attributes_for(:user, email: nil) }
+        expect(response).to render_template("new")
+      end
     end
   end
 end
