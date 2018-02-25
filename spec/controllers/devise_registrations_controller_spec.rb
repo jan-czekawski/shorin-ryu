0e43c47113e2e0c9bc396fae414eed86c43a8e85
Michal <bihone88@gmail.com>
2018-02-25 14:46:26 +0000

    Update devise:registration controller spec - fix correct user params

--- spec/controllers/devise_registrations_controller_spec.rb
+++ spec/controllers/devise_registrations_controller_spec.rb
@@ -2,27 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Devise::RegistrationsController, type: :controller do
   
-  # after(:all) do
-  #   User.delete_all
-  # end
+  after(:all) do
+    User.delete_all
+  end
   
-  # before(:each) do
-  #   @request.env["devise.mapping"] = Devise.mappings[:user]
-  # end
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
   
   describe "Devise::RegistrationsController#create" do
-    # it "creates new user if valid information is provided", :dev do
-    #   # expect do
-    #   #   post :create, params: { user: { email: "first_devise@emailcom",
-    #   #                                   password: "password",
-    #   #                                   password_confirmation: "password" } }
-    #   # end.to change(User, :count).by(1)
-    #   expect do
-    #     User.create(email: "devise@email.com",
-    #                 login: "devise",
-    #                 password: "password",
-    #                 password_confirmation: "password")
-    #   end.to change(User, :count).by(1)
-    # end
+    it "creates new user if valid information is provided" do
+      expect do
+        post :create, params: { user: attributes_for(:user) }
+      end.to change(User, :count).by(1)
+    end
   end
 end
