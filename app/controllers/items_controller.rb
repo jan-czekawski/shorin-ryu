c0fd1d4506ed93b57e073dbd74c7263876d6c6a8
Michal <bihone88@gmail.com>
2018-02-27 22:19:02 +0000

    Add require admin callback to item controllers methdos.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[show edit update destroy]
   before_action :require_user, only: %i[new create edit update destroy]
+  before_action :require_admin, only: %i[new create edit update destroy]
 
   def index
     @items = Item.all
