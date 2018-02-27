f595deb5e2a83f185446ac5527d930eccd4cac0d
Michal <bihone88@gmail.com>
2018-02-27 22:19:37 +0000

    Update item#new in items controller spec.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ItemsController, type: :controller do
 
   before(:all) do
     @user = create(:user)
+    @admin = create(:admin)
     @item = create(:item)
   end
   
@@ -33,19 +34,32 @@ RSpec.describe ItemsController, type: :controller do
 
   describe "#new", :new do
     context "when user logged in" do
-      
-      before(:each) do
-        sign_in @user
+      describe "and admin" do
+        before(:each) do
+          sign_in @admin
+        end
+        
+        it "renders new template" do
+          get :new
+          expect(response).to render_template :new
+        end
+        
+        it "assigns new instance of Item to @item" do
+          get :new
+          expect(assigns(:item)).to be_a_new Item
+        end
       end
       
-      it "renders new template" do
-        get :new
-        expect(response).to render_template :new
-      end
-      
-      it "assigns new instance of Item to @item" do
-        get :new
-        expect(assigns(:item)).to be_a_new Item
+      describe "and not admin" do
+        
+        before(:each) do
+          sign_in @user
+        end
+        
+        it "redirect_to root url" do
+          get :new
+          expect(response).to redirect_to root_url
+        end
       end
     
     end
@@ -60,35 +74,67 @@ RSpec.describe ItemsController, type: :controller do
 
   describe "#create", :new do
     context "when user logged in" do
-      
-      before(:each) do
-        sign_in @user
-      end
-      
-      describe "with valid information" do
-        it "increases item count by 1" do
-          expect do
-            post :create, params: { item: attributes_for(:item) }
-          end.to change(Item, :count).by(1)
+      describe "and admin" do
+        before(:each) do
+          sign_in @admin
         end
         
-        it "redirects to items path" do
-          post :create, params: { item: attributes_for(:item) }
-          expect(response).to redirect_to items_url
+        context "with valid information" do
+          it "increases item count by 1" do
+            expect do
+              post :create, params: { item: attributes_for(:item) }
+            end.to change(Item, :count).by(1)
+          end
+          
+          it "redirects to items path" do
+            post :create, params: { item: attributes_for(:item) }
+            expect(response).to redirect_to items_url
+          end
+        end
+        
+        context "with invalid information" do
+          it "doesnt' change item count" do
+            expect do
+              post :create, params: { item: attributes_for(:item, name: nil) }
+            end.not_to change(Item, :count)
+          end
+          
+          it "renders new item template" do
+            post :create, params: { item: attributes_for(:item, name: nil) }
+            expect(response).to render_template :new
+          end
         end
       end
       
-      describe "with invalid information" do
-        it "doesnt' change item count" do
+      describe "and not admin" do
+        
+        before(:each) do
+          sign_in @user
+        end
+        
+        it "doesn't change item count" do
           expect do
-            post :create, params: { item: attributes_for(:item, name: nil) }
+            post :create, params: { item: attributes_for(:item) }
           end.not_to change(Item, :count)
         end
         
-        it "renders new item template" do
-          post :create, params: { item: attributes_for(:item, name: nil) }
-          expect(response).to render_template :new
-        end
+        it "redirects to root url" do
+          post :create, params: { item: attributes_for(:item) }
+          expect(response).to redirect_to root_url
+        end        
+      end
+    end
+    
+    context "when user not logged in" do
+      it "doesn't change item count" do
+        expect do
+          post :create, params: { item: attributes_for(:item) }
+        end.not_to change(Item, :count)
+      end
+      
+      it "redirects to login page" do
+        post :create, params: { item: attributes_for(:item) }
+        expect(response).to require_login
       end
     end
     
