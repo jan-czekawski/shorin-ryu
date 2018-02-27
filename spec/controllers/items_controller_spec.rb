158aa508cf8d9359df613bc4e141499077c07f61
Michal <bihone88@gmail.com>
2018-02-27 21:55:15 +0000

    Add require login callback to item controller.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,45 +3,74 @@ require 'rails_helper'
 RSpec.describe ItemsController, type: :controller do
 
   before(:all) do
+    @user = create(:user)
     @item = create(:item)
   end
   
   after(:all) do
+    User.delete_all
     Item.delete_all
   end
 
   describe "#index" do
-    it "returns http success" do
+    it "assigns array of all items to @items" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(assigns(:items)).to match_array([@item])
+    end
+    
+    it "renders index template" do
+      get :index
+      expect(response).to render_template :index
     end
   end
 
   describe "#show" do
-    it "returns http success" do
+    it "renders show template" do
       get :show, params: { id: @item.id }
-      expect(response).to have_http_status(:success)
+      expect(response).to render_template :show
     end
   end
 
-  describe "#new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  describe "#new", :new do
+    context "when user logged in" do
+      
+      before(:each) do
+        sign_in @user
+      end
+      
+      it "renders new template" do
+        get :new
+        expect(response).to render_template :new
+      end
+      
+      it "assigns new instance of Item to @item" do
+        get :new
+        expect(assigns(:item)).to be_a_new Item
+      end
+    
+    end
+    
+    context "when user not logged in" do
+      it "redirects to login page" do
+        get :new
+        expect(response).to require_login
+      end
     end
   end
 
-  # describe "#create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "#create" do
+    context "when user logged in" do
+      it "returns http success" do
+        # get :create
+        # expect(response).to have_http_status(:success)
+      end
+    end
+  end
 
   describe "#edit" do
     it "returns http success" do
-      get :edit, params: { id: @item.id }
-      expect(response).to have_http_status(:success)
+      # get :edit, params: { id: @item.id }
+      # expect(response).to have_http_status(:success)
     end
   end
 
