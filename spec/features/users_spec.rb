9cb2d40d4b8fa202b760e2d32efaa78a375c0f76
Michal <bihone88@gmail.com>
2018-02-27 10:55:31 +0000

    Update new user scenario in user management feature spec

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -6,12 +6,21 @@ feature "User management", :new do
   end
   
   scenario "adds a new user" do
-    user = create(:user)
+    user = build(:user)
     
     visit root_path
-    click_link "Sign in"
-    fill_in "Email", with: user.email 
-    fill_in "Password", with: user.password
-    click_button "Log in"
+    click_link "Sign up"
+    expect{
+      fill_in "Email", with: user.email 
+      fill_in "Login", with: user.login 
+      fill_in "Password", with: user.password
+      fill_in "Password confirmation", with: user.password
+      click_button "Create"
+    }.to change(User, :count).by(1)
+    expect(current_path).to eq root_path
+    expect(page).to have_content "You have signed up successfully."
+    within "li.nav-item.dropdown" do
+      "Signed in as #{user.login}"
+    end
   end
 end
