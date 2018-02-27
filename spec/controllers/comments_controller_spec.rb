8742580e5f260fc7f2f99212debce8226721a3cb
Michal <bihone88@gmail.com>
2018-02-27 20:21:37 +0000

    Finish comment#create controller spec.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -17,21 +17,37 @@ RSpec.describe CommentsController, type: :controller do
   describe "#post", :new do
     
     context "when user logged in" do
+      before(:each) do
+        sign_in @user
+      end
       
       describe "with valid event information" do
         it "increases comment count by 1" do
-          sign_in @user
           expect do
             post :create, params: { event_id: @event.id,
                                     comment: { content: "random" } }
           end.to change(Comment, :count).by(1)
         end
         
-        it "redirects to event page after creation" do
-          sign_in @user
+        it "redirects to event page after comment creation" do
           post :create, params: { event_id: @event.id,
                                   comment: { content: "random" } }
           expect(response).to redirect_to event_path(@event)
+        end
+      end
+      
+      describe "with valid item information" do
+        it "increases comment count by 1" do
+          expect do
+            post :create, params: { item_id: @item.id,
+                                    comment: { content: "random" } }
+          end.to change(Comment, :count).by(1)
+        end
+        
+        it "redirects to item page after comment creation" do
+          post :create, params: { item_id: @item.id,
+                                  comment: { content: "random" } }
+          expect(response).to redirect_to item_path(@item)
         end
       end
       
