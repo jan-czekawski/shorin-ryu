8a2788aa4d7c00c275096642aa24bdb858893898
Michal <bihone88@gmail.com>
2018-02-27 19:15:08 +0000

    Add delete event to event feature spec.

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -7,12 +7,13 @@ feature "Events handling", type: :feature do
   end
   
   before(:all) do
-    @user = create(:user)
+    @admin = create(:admin)
+    @event = create(:event)
   end
   
   scenario "add an event" do
     event = build(:event)
-    login_as(@user.email, @user.password)
+    login_as(@admin.email, @admin.password)
     click_link "Events"
     expect(current_path).to eq events_path
     click_link "New event"
@@ -30,18 +31,42 @@ feature "Events handling", type: :feature do
   end
   
   scenario "display all events" do
-    first_event = Event.first
     visit root_path
     click_link "Events"
     expect(current_path).to eq events_path
     Event.each do |event|
       check_content(event)
     end
-    click_link "Show", href: event_path(first_event)
-    check_content(first_event)
-    expect(page).to have_content first_event.comments if first_event.comments.any? 
-    expect(page).to have_content first_event.user.login
-    expect(page).to have_content first_event.created_at.strftime("%e-%b-%Y")
+    click_link "Show", href: event_path(@event)
+    check_content(@event)
+    expect(page).to have_content @event.comments if @event.comments.any? 
+    expect(page).to have_content @event.user.login
+    expect(page).to have_content @event.created_at.strftime("%e-%b-%Y")
+  end
+  
+  scenario "update an event" do
+    login_as(@admin.email, @admin.password)
+    click_link "Events"
+    click_link "Edit", href: edit_event_path(@event)
+    fill_in "Name", with: "New Event Nam"
+    fill_in "Street", with: "Quarter Street"
+    fill_in "House #", with: 32
+    fill_in "City", with: "Los Alamos"
+    fill_in "Zip code", with: "22-333"
+    click_button "Update"
+    expect(current_path).to eq event_path(@event)
+    expect(page).to have_content "Event was successfully updated!"
+  end
+  
+  scenario "delete an event" do
+    login_as(@admin.email, @admin.password)
+    click_link "Events"
+    click_link "Show", href: event_path(@event)
+    expect {
+      click_link "Delete"
+    }.to change(Event, :count).by(-1)
+    expect(current_path).to eq events_path
+    expect(page).to have_content "Event was successfully deleted!"
   end
   
   def check_content(event)
