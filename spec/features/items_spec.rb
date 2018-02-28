bf0b79df6f56b1d54961bebec117199333434e35
Michal <bihone88@gmail.com>
2018-02-28 13:26:42 +0000

    Finish add item scenario in feature spec.

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -11,13 +11,14 @@ feature "Items handling", type: :feature do
     @item = create(:item)
   end
   
-  scenario "add item" do
+  scenario "add a item" do
     item = build(:item)
     visit root_path
     login_as(@admin.email, @admin.password)
     click_link "Shop"
     expect(current_path).to eq items_path
     click_link "New item"
+    expect(current_path).to eq new_item_path
     expect {
       fill_in "Name", with: item.name 
       fill_in "Description", with: item.description
@@ -26,5 +27,7 @@ feature "Items handling", type: :feature do
       fill_in "Item_id", with: item.store_item_id
       click_button "Create" 
     }.to change(Item, :count).by(1)
+    expect(current_path).to eq items_path
+    expect(page).to have_content "New item has been created."
   end
 end
