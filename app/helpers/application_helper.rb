f38e12b634981dd0d75afbf12392597e48abdb3e
michal8888 <bihone@wp.pl>
2018-03-01 18:54:35 +0100

    Update few helpers based on guidelines.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,23 @@
 module ApplicationHelper
   def show_avatar(user, size = 100)
-    return image_tag(user.image, alt: user.email, size: 100, class: "avatar") unless user.image.blank?
+    unless user.image.blank?
+      return image_tag(user.image, alt: user.email, size: 100, class: "avatar")
+    end
     id = Digest::MD5.hexdigest(user.email.downcase)
     path = "https://secure.gravatar.com/avatar/#{id}?s=#{size}&d=mm"
     image_tag(path, alt: user.email, class: "avatar")
   end
-  
+
   def extract_name(user)
     # name = user.email.split(/@/)[0]
     name = user.email.slice(/\w+(?=@)/)
     truncate(name, length: 15)
   end
-  
+
   def current_user?(user)
     user == current_user
   end
-  
+
   def owner_or_admin?(user, resource)
     return false unless user_signed_in?
     user.admin? || (resource.user_id == user._id)
--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,7 +2,9 @@ module DeviseHelper
   def devise_error_messages!
     return "" unless devise_error_messages?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    messages = resource.errors.full_messages.map do |msg|
+      content_tag(:li, msg)
+    end.join
     # sentence = I18n.t("errors.messages.not_saved",
     #                   count: resource.errors.count,
     #                   resource: resource.class.model_name.human.downcase)
