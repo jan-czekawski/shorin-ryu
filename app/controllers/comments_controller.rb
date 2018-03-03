7c275424c3618a43cfe8a58da8a595f542dc101a
Michal <bihone88@gmail.com>
2018-03-03 15:27:15 +0000

    Refactor comment controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,22 +2,18 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:destroy]
   before_action :require_user, only: %i[create destroy]
   before_action :require_owner_or_admin, only: [:destroy]
-
+  
   include Commentable
 
   def create
     @commentable = find_commentable
-    path = @commentable.is_a?(Event) ? event_path(@commentable) : item_path(@commentable)
     @comment = @commentable.comments.build(comment_params)
     @comment.user_id = current_user.id
     
     if @comment.save
       flash[:success] = "New comment has been added!"
     else
-      flash[:alert] = "There is an error. "
-      @comment.errors.full_messages.each do |message|
-        flash[:alert] += message + "."
-      end
+      flash[:alert] = @comment.display_errors
     end
     redirect_to @commentable
   end
