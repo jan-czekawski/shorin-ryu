7375e5a3c3e492cfe1ad54072f2cf89b8ee32158
Michal <bihone88@gmail.com>
2018-03-04 10:56:39 +0000

    Redefine login_as - eliminate duplicate with warden's method.

--- spec/support/macros/login_macros.rb
+++ spec/support/macros/login_macros.rb
@@ -1,5 +1,5 @@
 module LoginMacros
-  def login_as(email, password = "password")
+  def login_user(email, password = "password")
     visit root_path
     click_link "Sign in"
     fill_in "Email", with: email
