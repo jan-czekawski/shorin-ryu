9952cfbe29659186e7e4dc5f80b07c415808b797
Michal <bihone88@gmail.com>
2018-03-05 06:34:10 +0000

    Add cart item update spec

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,5 +1,6 @@
 class CartItemsController < ApplicationController
   before_action :set_cart, only: [:create]
+  before_action :set_cart_item, only: %i[update, destroy]
 
   def create
     item = @cart.in_cart_already(cart_items_params[:item_id])
@@ -13,14 +14,27 @@ class CartItemsController < ApplicationController
       redirect_back fallback_location: items_path
     end
   end
+  
+  def update
+    if @cart_item.update(cart_items_params)
+      # flash[:success] = "Item's quantity has been updated."
+      # redirect_to cart_path()
+    else
+      
+    end
+  end
 
   def destroy
-    CartItem.find(params[:id]).delete
+    @cart_item.delete
     flash[:success] = "Item has been deleted from your cart."
     redirect_back fallback_location: items_path
   end
   
   private
+
+  def set_cart_item
+    @cart_item = CartItem.find(params[:id])
+  end
 
   def cart_items_params
     params.require(:cart_item).permit(:quantity, :item_id)
--- spec/controllers/cart_items_controller_spec.rb
+++ spec/controllers/cart_items_controller_spec.rb
@@ -11,31 +11,61 @@ RSpec.describe CartItemsController, type: :controller do
       before(:each) { sign_in @cart.user }
       
       describe "with valid item information" do
-        it "increases cart item count by 1" do
-          expect do
+        context "when item not in the cart" do
+          it "increases cart item count by 1" do
+            expect do
+              post :create, params: { cart_id: @cart.id,
+                                      cart_item: { quantity: 3,
+                                                   item_id: @item.id } }
+            end.to change(CartItem, :count).by(1)
+          end
+          
+          it "redirects to cart after creation" do
             post :create, params: { cart_id: @cart.id,
-                                    cart_item: { quantity: 3,
+                                    cart_item: { quantity: 20,
                                                  item_id: @item.id } }
-          end.to change(CartItem, :count).by(1)
+            expect(response).to redirect_to @cart
+          end
         end
         
-        it "redirects to cart after creation" do
-          post :create, params: { cart_id: @cart.id,
-                                  cart_item: { quantity: 20,
-                                               item_id: @item.id } }
-          expect(response).to redirect_to @cart
+        context "when item already in the cart" do
+          it "increases items quantity in the cart" do
+            last_item = @cart.cart_items.last
+            expect do
+              post :create, params: { cart_id: @cart.id,
+                                      cart_item: { quantity: 10,
+                                                   item_id: @item.id } }
+              last_item.reload
+            end.to change(last_item, :quantity).by(10)
+          end
         end
       end
       
       describe "with invalid item information" do
-        it "doesn't change cart item count" do
-          last_item = @cart.cart_items.last
+        it "doesn't change cart items count" do
           expect do
             post :create, params: { cart_id: @cart.id,
                                     cart_item: { quantity: 10,
-                                                 item_id: @item.id } }
+                                                 item_id: nil } }
+          end.not_to change(CartItem, :count)
+        end
+      end
+    end
+  end
+  
+  describe "#update" do
+    context "when user logged in" do
+      before(:each) { sign_in @cart.user }
+      
+      describe "when item already in the cart" do
+        it "updates items quantity" do
+          last_item = @cart.cart_items.last
+          expect do
+            patch :update, params: {   cart_id: @cart.id,
+                                            id: last_item.id,
+                                     cart_item: { quantity: 5 } }
             last_item.reload
-          end.to change(last_item, :quantity).by(10)
+          end.to change(last_item, :quantity).by(-5)
         end
       end
     end
