4274a3663b5ed30d5181b3ea134724ee4d13d572
Michal <bihone88@gmail.com>
2018-03-11 22:04:07 +0000

    Add contexts to comments controller spec to handle deleting comment when user admin, owner or not owner nor admin.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -53,29 +53,108 @@ RSpec.describe CommentsController, type: :controller do
     end
 
     context "when user logged in" do
-      describe "with valid event information" do
-        it "decreases comment count by 1 and redirects to event page" do
-          # TODO: check comment_id vs event_id and .. vs item_id
-          comment = create(:events_comment)
-          sign_in comment.user
-          expect do
-            delete :destroy, params: { id: comment.id,
-                                       event_id: comment.commentable_id }
-          end.to change(Comment, :count).by(-1)
-          expect(response).to redirect_to event_path(comment.commentable_id)
+      context "and is comment's owner" do
+        describe "with valid event information" do
+          it "decreases comment count by 1 and redirects to event page" do
+            # TODO: check comment_id vs event_id and .. vs item_id
+            comment = create(:events_comment)
+            sign_in comment.user
+            expect do
+              delete :destroy, params: { id: comment.id,
+                                         event_id: comment.commentable_id }
+            end.to change(Comment, :count).by(-1)
+            expect(response).to redirect_to event_path(comment.commentable_id)
+          end
+        end
+  
+        describe "with valid item information" do
+          it "decreases comment count by 1 and redirects to item page" do
+            comment = create(:items_comment)
+            sign_in comment.user
+            expect do
+              delete :destroy, params: { id: comment.id,
+                                         item_id: comment.commentable_id }
+            end.to change(Comment, :count).by(-1)
+            expect(response).to redirect_to item_path(comment.commentable_id)
+          end
+        end
+        
+        describe "when comment's id and event_id are not from the same comment" do
+          it "doesn't change comment count and redirects to event page" do
+            comment = create(:events_comment)
+            diff_event = create(:event)
+            sign_in comment.user
+            expect do
+              delete :destroy, params: { id: comment.id,
+                                         event_id: diff_event.id }
+            end.not_to change(Comment, :count)
+            expect(response).to redirect_to root_url
+          end
+        end
+        
+        describe "when comment's id and item_id are not from the same comment" do
+          it "doesn't change comment count and redirects to item page" do
+            comment = create(:items_comment)
+            diff_item = create(:item)
+            sign_in comment.user
+            expect do
+              delete :destroy, params: { id: comment.id,
+                                         item_id: diff_item.id }
+            end.not_to change(Comment, :count)
+            expect(response).to redirect_to root_url
+          end
         end
       end
-
-      describe "with valid item information" do
-        # TODO: handle deleting only if user is owner's comme
-        it "decreases comment count by 1" do
-          comment = create(:items_comment)
-          sign_in comment.user
-          expect do
-            delete :destroy, params: { id: comment.id,
-                                       item_id: comment.commentable_id }
-          end.to change(Comment, :count).by(-1)
-          expect(response).to redirect_to item_path(comment.commentable_id)
+      
+      context "and is admin" do
+        describe "with valid event information" do
+          it "decreases comment count by 1 and redirects to event page" do
+            comment = create(:events_comment)
+            sign_in create(:admin)
+            expect do
+              delete :destroy, params: { id: comment.id,
+                                         event_id: comment.commentable_id }
+            end.to change(Comment, :count).by(-1)
+            expect(response).to redirect_to event_path(comment.commentable_id)
+          end
+        end
+  
+        describe "with valid item information" do
+          it "decreases comment count by 1 and redirects to item page" do
+            comment = create(:items_comment)
+            sign_in create(:admin)
+            expect do
+              delete :destroy, params: { id: comment.id,
+                                         item_id: comment.commentable_id }
+            end.to change(Comment, :count).by(-1)
+            expect(response).to redirect_to item_path(comment.commentable_id)
+          end
+        end
+      end
+      
+      context "and not comment's owner nor admin" do
+        describe "with valid event information" do
+          it "doesn't change comment count and redirects to root" do
+            comment = create(:events_comment)
+            sign_in create(:user)
+            expect do
+              delete :destroy, params: { id: comment.id,
+                                         event_id: comment.commentable_id }
+            end.not_to change(Comment, :count)
+            expect(response).to redirect_to root_url
+          end
+        end
+        
+        describe "with valid item information" do
+          it "doesn't change comment count and redirects to root" do
+            comment = create(:items_comment)
+            sign_in create(:user)
+            expect do
+              delete :destroy, params: { id: comment.id,
+                                         item_id: comment.commentable_id }
+            end.not_to change(Comment, :count)
+            expect(response).to redirect_to root_url
+          end
         end
       end
     end
