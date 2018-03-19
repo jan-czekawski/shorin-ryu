4e5ccc3db62a063bc2e35125c9c9e3e1dec20cef
Michal <bihone88@gmail.com>
2018-03-19 00:10:05 +0000

    Update checkout controller - add edit update and remove new create.

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,8 +1,26 @@
 class CheckoutsController < ApplicationController
-  def new
-    @checkout = Checkout.new
+  before_action :set_checkout, only: %i[edit update]
+  
+  def edit
+  end
+  
+  def update
   end
 
   def show
+  end
+  
+  private
+
+  def set_checkout
+    p Checkout.count
+    p Checkout.last.id
+    @checkout = Checkout.find(params[:id])
+  end
+
+  def event_params
+    params.require(:checkout).permit(:name, :user_id, :image,
+                                  address_attributes: %i[city street
+                                                         house_number zip_code])
   end
 end
