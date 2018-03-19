1cff29295b85f77df8dd0f4a1562fafebd2c158c
Michal <bihone88@gmail.com>
2018-03-19 00:11:48 +0000

    Update show, edit checkout template.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  
-
   resources "carts", only: %i[show] do
     resources "cart_items", only: %i[create update destroy], as: :items
   end
   
-  resources "checkouts", only: %i[show new create]
+  resources "checkouts", only: %i[show edit update]
 
   devise_for :users
   resources "users", only: %i[index show destroy]
--- spec/features/checkout_spec.rb
+++ spec/features/checkout_spec.rb
@@ -7,23 +7,35 @@ feature "Handle orders in checkout", type: :feature do
     user = build(:user)
     cart = build(:cart, user: user)
     item = build(:item)
-    cart_item = create(:cart_item, item: item, cart: cart)
+    create(:cart_item, item: item, cart: cart)
     login_as(user)
     visit cart_path(cart)
     click_link "Proceed to checkout"
-    expect(current_path).to eq new_checkout_path
+    expect(current_path).to eq edit_checkout_path(cart.checkout)
     expect(page).to have_content "Items in your checkout:"
     expect(page).to have_content cart.sum_price
-    check "Standard delivery"
-    click_button "Continue"
-    fill_in "First Name", with: "John"
-    fill_in "Last Name", with: "Doe"
-    fill_in "Phone Number", with: "+48500800800"
-    fill_in "Email Address", with: user.email
-    fill_in "City", with: "Juarez"
-    fill_in "Street", with: "Main Street"
-    fill_in "House #", with: "100"
-    fill_in "Zipcode", with: "22-300"
-    click_button "Continue"
+    choose "Standard delivery"
+    # click_button "Continue"
+    within ".delivery_address" do
+      fill_in "First Name", with: "John"
+      fill_in "Last Name", with: "Doe"
+      fill_in "Phone Number", with: "+48500800800"
+      fill_in "Email Address", with: user.email
+      fill_in "City", with: "Juarez"
+      fill_in "Street", with: "Main Street"
+      fill_in "House #", with: "100"
+      fill_in "Zipcode", with: "22-300"
+    end
+    within ".billing_address" do
+      fill_in "First Name", with: "John"
+      fill_in "Last Name", with: "Doe"
+      fill_in "Phone Number", with: "+48500800800"
+      fill_in "Email Address", with: user.email
+      fill_in "City", with: "Juarez"
+      fill_in "Street", with: "Main Street"
+      fill_in "House #", with: "100"
+      fill_in "Zipcode", with: "22-300"
+    end
+    # click_button "Continue"
   end
 end
