37f90b9ed9fa4c1a7719d44d712fda1b84f9b5b1
Michal <bihone88@gmail.com>
2018-03-21 19:40:38 +0000

    Update checkout feature spec - to pass handling clicking button to copy delivery address.

--- spec/features/checkout_spec.rb
+++ spec/features/checkout_spec.rb
@@ -3,7 +3,7 @@ include Warden::Test::Helpers
 Warden.test_mode!
 
 feature "Handle orders in checkout", type: :feature do
-  scenario "move items from the cart to checkout" do
+  scenario "move items from the cart to checkout", js: true do
     user = build(:user)
     cart = build(:cart, user: user)
     item = build(:item)
@@ -28,14 +28,14 @@ feature "Handle orders in checkout", type: :feature do
     end
     click_button "Use delivery address"
     within ".billing_address" do
-    #   fill_in "First Name", with: "John"
-    #   fill_in "Last Name", with: "Doe"
-    #   fill_in "Phone Number", with: "+48500800800"
-    #   fill_in "Email Address", with: user.email
-    #   fill_in "City", with: "Juarez"
-    #   fill_in "Street", with: "Main Street"
-    #   fill_in "House #", with: "100"
-    #   fill_in "Zipcode", with: "22-300"
+      expect(page).to have_field("First Name", with: "John")
+      expect(page).to have_field("Last Name", with: "Doe")
+      expect(page).to have_field("Phone Number", with: "+48500800800")
+      expect(page).to have_field("Email Address", with: user.email)
+      expect(page).to have_field("City", with: "Juarez")
+      expect(page).to have_field("Street", with: "Main Street")
+      expect(page).to have_field("House #", with: "100")
+      expect(page).to have_field("Zipcode", with: "22-300")
     end
     # click_button "Continue"
   end
