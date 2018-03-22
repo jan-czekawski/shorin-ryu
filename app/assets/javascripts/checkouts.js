3b10bb8397cec285688b3bd99f7bc3dcd981334c
Michal <bihone88@gmail.com>
2018-03-22 13:51:05 +0000

    Add functions to handle continue and back buttons.

--- app/assets/javascripts/checkouts.js
+++ app/assets/javascripts/checkouts.js
@@ -4,6 +4,43 @@ $(document).on("turbolinks:load", function(){
     CollectDeliveryAddress(array);
     PasteBillingAddress(array);
   });
+  
+  $("#pick_delivery_method").on("click", function(){
+    $(".delivery_method").hide();
+    $(".delivery_address").show();
+    $("#back_delivery_address").show();
+    $("#pick_delivery_address").show();
+    $(this).hide();
+  });
+  
+  $("#back_delivery_address").on("click", function(){
+    $(".delivery_method").show();
+    $(".delivery_address").hide();
+    $("#back_delivery_address").hide();
+    $("#pick_delivery_address").hide();
+    $(this).hide();
+    $("#pick_delivery_method").show();
+  });
+
+  $("#pick_delivery_address").on("click", function(){
+    $(".billing_address").show();
+    $(".delivery_address").hide();
+    $("#use_delivery_address").show();
+    $("#submit_form").show();
+    $(this).hide();
+    $("#back_delivery_address").hide();
+    $("#back_billing_address").show();
+  });
+  
+  $("#back_billing_address").on("click", function(){
+    $(".billing_address").hide();
+    $(".delivery_address").show();
+    $("#use_delivery_address").hide();
+    $("#submit_form").hide();
+    $(this).hide();
+    $("#back_delivery_address").show();
+    $("#pick_delivery_address").show();
+  });
 });
 
 function CollectDeliveryAddress(arr){
