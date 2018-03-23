83d8bdfaafee573a9f9f3425eae8d30965b167be
Michal <bihone88@gmail.com>
2018-03-23 21:51:24 +0000

    Update development config - force ssl

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,10 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Force all access to the app over SSL, use Strict-Transport-Security
+  # , and use secure cookies.
+  config.force_ssl = true
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
