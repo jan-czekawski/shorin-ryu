f3b82b6705802d87f93352fdb2d1d8fa354a5d8d
Michal <bihone88@gmail.com>
2018-03-24 12:36:39 +0000

    Add paid boolean to checkouts model.

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,5 +1,6 @@
 class Checkout
   include Mongoid::Document
+  # field :paid, type: Boolean, default: false
   field :delivery_method, type: Integer
   field :payment_method, type: Integer
   field :delivery_address, type: CheckoutAddress
