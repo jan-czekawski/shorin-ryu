0015f66f3f50cde64f7c3c6fda4ef54e9ad9c2a0
Michal <bihone88@gmail.com>
2018-03-30 16:01:18 +0000

    Define method to move cart items from cart to checkout - to be fixed

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -5,9 +5,18 @@ class Checkout
   field :payment_method, type: Integer
   field :delivery_address, type: CheckoutAddress
   field :billing_address, type: CheckoutAddress
+  has_many :cart_items, dependent: :destroy
+  
   belongs_to :user
 
   def move_items_from_cart
-    
+    # byebug
+    # TODO: make sure why method is missing - is cart deleted before or unavailable in checkout???
+    c_items = []
+    self.user.cart.cart_items.each do |c_item|
+      c_items.push(c_item)
+      # self.cart_items.push(c_item)
+    end
+    self.update(cart_items: c_items)
   end
 end
