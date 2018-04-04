cb5938d5f4fceb97abf467d9219d4ae3e6ac0da9
Michal <bihone88@gmail.com>
2018-04-04 10:31:11 +0000

    Add checkout#new controller spec.

--- spec/controllers/checkouts_controller_spec.rb
+++ spec/controllers/checkouts_controller_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe CheckoutsController, type: :controller do
-
   # TODO: set up checkouts controller
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
+  describe "#new" do
+    context "when user logged in" do
+      
+      it "renders new template and assigns new checkout instance to @checkout" do
+        sign_in create(:user)
+        get :new
+        expect(response).to render_template :new
+        expect(assigns(:checkout)).to be_a_new Checkout
+      end
+    end
+    
+    context "when user not logged in" do
+    end
+  end
 end
