a85feb79c7d937f9a18edeede642efb33a773113
Michal <bihone88@gmail.com>
2018-04-11 21:07:58 +0000

    Add todo comment to fix checkout spec.

--- spec/features/checkout_spec.rb
+++ spec/features/checkout_spec.rb
@@ -12,6 +12,7 @@ feature "Handle orders in checkout", type: :feature do
     visit cart_path(cart)
     click_link "Proceed to checkout"
     expect(current_path).to eq new_checkout_path
+    # TODO: fix checkout spec later
     expect(page).to have_content "Items in your checkout:"
     expect(page).to have_content cart.sum_price
     choose "Standard delivery"
