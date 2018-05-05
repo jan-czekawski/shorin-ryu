7323bbad0fbfb5e359b153172ccea209442ee5df
Michal <bihone88@gmail.com>
2018-05-05 18:34:24 +0000

    Update show_avatar helper - pass user.image.url to image_tag method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def show_avatar(user, size = 100)
     unless user.image.blank?
-      return image_tag(user.image, alt: user.email, size: 100, class: "avatar")
+      return image_tag(user.image.url, alt: user.email, size: 100, class: "avatar")
     end
     id = Digest::MD5.hexdigest(user.email.downcase)
     path = "https://secure.gravatar.com/avatar/#{id}?s=#{size}&d=mm"
