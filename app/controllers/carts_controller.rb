778ce039f09e43857d85aa2ba6a6533cffb8cfcf
Michal <bihone88@gmail.com>
2018-05-19 23:14:43 +0000

    Add comment to refactor rendering cart in sidebar.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,8 @@ class CartsController < ApplicationController
   before_action :set_cart, only: [:show]
 
   def show; end
+    
+  # TODO: add rendering content of the cart in the sidebar => <%= j render @cart %>
 
   private
 
