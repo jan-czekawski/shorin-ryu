f233f7c7a40d823e5970df609413eb72f547e51e
Michal <bihone88@gmail.com>
2018-05-29 10:37:34 +0000

    Remove white spaces from lib files.

--- lib/checkout_address.rb
+++ lib/checkout_address.rb
@@ -1,23 +1,22 @@
 class CheckoutAddress
   attr_reader :city, :street, :house_number, :zip_code
-  
+
   def initialize(city, street, house_number, zip_code)
     @city = city
     @street = street
     @house_number = house_number
     @zip_code = zip_code
   end
-  
+
   def mongoize
     [city, street, house_number, zip_code]
   end
-  
+
   class << self
-    
     def demongoize(object)
       CheckoutAddress.new(object[0], object[1], object[2], object[3])
     end
-    
+
     def mongoize(object)
       case object
       when CheckoutAddress
@@ -30,7 +29,7 @@ class CheckoutAddress
         object
       end
     end
-    
+
     def evolve(object)
       case object
       when CheckoutAddress
