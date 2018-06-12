d48d347c553cb4d910064d82b00a550882bfd205
Michal <bihone88@gmail.com>
2018-06-12 19:33:43 +0000

    Style cart_requirements file

--- lib/cart_requirements.rb
+++ lib/cart_requirements.rb
@@ -7,13 +7,13 @@ module CartRequirements
   def cart_items_params
     params.require(:cart_item).permit(:quantity, :item_id)
   end
-  
+
   def require_same_cart
     return if current_user.cart == @cart_params
     flash[:alert] = "You can only access your own cart."
     redirect_to current_user.cart
   end
-  
+
   def require_item_in_cart
     return if current_user.cart.cart_items.include?(@cart_item)
     flash[:alert] = "Item wasn't in your cart."
