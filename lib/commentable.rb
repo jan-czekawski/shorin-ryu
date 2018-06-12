61d7642b819d31d274cbda342e2bb1b65193fe6e
Michal <bihone88@gmail.com>
2018-06-12 19:31:30 +0000

    Style lib/commentable file

--- lib/commentable.rb
+++ lib/commentable.rb
@@ -1,14 +1,14 @@
 module Commentable
   def find_commentable
     params.each do |name, value|
-      # return $1.classify.constantize.find(value) if name =~ /(.+)_id/
-      return Object.const_get($1.capitalize).find(value) if name =~ /(.+)_id/
+      return $1.classify.constantize.find(value) if name =~ /(.+)_id/
+      # return Object.const_get($1.capitalize).find(value) if name =~ /(.+)_id/
     end
   end
-  
+
   def require_valid_commentable_id
     @commentable = find_commentable
-    return if @comment.commentable_id == @commentable.id    
+    return if @comment.commentable_id == @commentable.id
     flash[:alert] = "#{@commentable.class}_id is not correct for that comment."
     redirect_to root_url
   end
