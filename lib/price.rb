8d8d82687c6b89dc8f6a597046398e3dd04db071
Michal <bihone88@gmail.com>
2018-06-22 03:22:54 +0000

    Revert back to selecting persisted cart_items in Price#sum_price - failed tests, so it still needs to be fixed.

--- lib/price.rb
+++ lib/price.rb
@@ -1,7 +1,9 @@
 module Price
   def sum_price
     sum = 0
-    cart_items.each do |c_item|
+    # TODO: fix @cart in items/show and items/index - to not include |
+    # not persisted cart_items in @cart
+    cart_items.select(&:persisted?).each do |c_item|
       sum += c_item.quantity * c_item.item.price
     end
     sum
